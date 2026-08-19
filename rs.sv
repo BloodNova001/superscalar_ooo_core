da8eee4fbe738260cc98614954d28acb5f4ecf07
Priyansh <ppriyansh001@gmail.com>
2026-08-19 11:49:35 +0530

    RS changes

--- rs.sv
+++ rs.sv
@@ -47,6 +47,17 @@ module rs (
             for (i = 0;i < $clog2(`RS_LSU_SIZE) ;i = i + 1 ) begin
                 free_lsu[i] <= ($clog2(`RS_LSU_SIZE))'(i);
             end
+
+            head_int <= $clog2(`RS_INT_SIZE)'d0;
+            tail_int <= $clog2(`RS_INT_SIZE)'d0;
+            head_mul <= $clog2(`RS_MUL_SIZE)'d0;
+            tail_mul <= $clog2(`RS_MUL_SIZE)'d0;
+            head_lsu <= $clog2(`RS_LSU_SIZE)'d0;
+            tail_lsu <= $clog2(`RS_LSU_SIZE)'d0;
+
+            free_int_counter <= $clog2(`RS_INT_SIZE)'d16;
+            free_mul_counter <= $clog2(`RS_MUL_SIZE)'d16;
+            free_lsu_counter <= $clog2(`RS_LSU_SIZE)'d16;
         end
         else begin
             for (i = 0;i < `DISPATCH_WIDTH ;i = i + 1 ) begin
@@ -58,14 +69,17 @@ module rs (
                         RS_int[tail_int] <= {uops_in[i][`UOP_ROB_TAG],uops_in[i][`UOP_ALU_OP],!uops_in[i][`UOP_Q1_VALID],uops_in[i][`UOP_Q1_TAG],value_in[i][31:0],!uops_in[i][`UOP_Q2_VALID],uops_in[i][`UOP_Q2_TAG],value_in[i][63:32]};
                     end
                     tail_int <= tail_int + 1;
+                    free_int_counter <= free_int_counter - 1;
                 end
                 else if (uops_in[i][`UOP_FU_TYPE] == `FU_MUL) begin
-                    RS_mul[tail_mul] <= {uops_in[i][`UOP_ROB_TAG],uops_in[i][`UOP_MUL_OP],!uops_in[i][`UOP_Q1_VALID],uops_in[i][`UOP_Q1_TAG],value_in[i][31:0],!uops_in[i][`UOP_Q2_VALID],uops_in[i][`UOP_Q2_TAG],value_in[i][63:32]};
+                    RS_mul[tail_mul] <= {uops_in[i][`UOP_ROB_TAG],uops_in[i][`UOP_ALU_OP],!uops_in[i][`UOP_Q1_VALID],uops_in[i][`UOP_Q1_TAG],value_in[i][31:0],!uops_in[i][`UOP_Q2_VALID],uops_in[i][`UOP_Q2_TAG],value_in[i][63:32]};
                     tail_mul <= tail_mul + 1;
+                    free_mul_counter <= free_mul_counter - 1;
                 end
                 else if (uops_in[i][`UOP_FU_TYPE] == `FU_LSU) begin
-                    RS_lsu[tail_lsu] <= {uops_in[i][`UOP_ROB_TAG],uops_in[i][`UOP_LSU_OP],!uops_in[i][`UOP_Q1_VALID],uops_in[i][`UOP_Q1_TAG],value_in[i][31:0],!uops_in[i][`UOP_Q2_VALID],uops_in[i][`UOP_Q2_TAG],value_in[i][63:32]};
+                    RS_lsu[tail_lsu] <= {uops_in[i][`UOP_ROB_TAG],uops_in[i][`UOP_ALU_OP],!uops_in[i][`UOP_Q1_VALID],uops_in[i][`UOP_Q1_TAG],value_in[i][31:0],!uops_in[i][`UOP_Q2_VALID],uops_in[i][`UOP_Q2_TAG],value_in[i][63:32]};
                     tail_lsu <= tail_lsu + 1;
+                    free_lsu_counter <= free_lsu_counter - 1;
                 end
             end
         end
